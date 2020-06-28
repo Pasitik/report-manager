6b317726688405f0e0b24d7deb55dd10e21cc317
bandoh <kelvinbandoh@hotmail.com>
2020-06-28 08:45:28 +0000

    maps and document transfer

--- src/views/Accidents/Accidents.js
+++ src/views/Accidents/Accidents.js
@@ -1,9 +1,9 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from 'react-router-dom';
-import firebase from "firebase"; 
+import firebase from "firebase";
 import geohash from "ngeohash";
-import {usePosition} from "use-position";
+import { usePosition } from "use-position";
 
 
 // @material-ui/core components
@@ -24,7 +24,7 @@ import { number } from "yup";
 
 //const useStyles = makeStyles(styles);
 
-export default function Accidents(props) { 
+export default function Accidents(props) {
   const StyledTableCell = withStyles((theme) => ({
     head: {
       backgroundColor: theme.palette.common.black,
@@ -34,32 +34,22 @@ export default function Accidents(props) {
       fontSize: 14,
     },
   }))(TableCell);
- 
-  const button = ( 
-    <Button
-      component={Link}
-      variant='contained'
-      color='primary'
-      size='small'
-      to={'/maps'}
-    >
-      View Map
-    </Button>
-  );
-  
-  const switchButton = (
-    <Button
-    component={Link}
-    variant='contained'
-    color='secondary'
-    size='small'
-    //to={'/maps'} 
-    
-  >
-    Solved
-  </Button>
-  );
-  
+
+  const updateContent = async (row) => {
+    const accRef = await db.collection("Accidents").doc(row.id).get()
+    if (accRef.exists) {
+      const data = accRef.data()
+
+      const solvedRef = await db.collection("SolvedAccidents").doc(row.id).get()
+      if (!solvedRef.exists) {
+        await db.collection("SolvedAccidents").doc(row.id).set(data).then(async (succ) => {
+          await db.collection("Accidents").doc(row.id).delete()
+        })
+      }
+    }
+  }
+
+
   const StyledTableRow = withStyles((theme) => ({
     root: {
       '&:nth-of-type(odd)': {
@@ -67,30 +57,30 @@ export default function Accidents(props) {
       },
     },
   }))(TableRow);
-  
-  const getGeohashRange = ( 
-    latitude= number,
-    longitude= number,
+
+  const getGeohashRange = (
+    latitude = number,
+    longitude = number,
     distance = number, // miles
   ) => {
     const lat = 0.0144927536231884; // degrees latitude per mile
     const lon = 0.0181818181818182; // degrees longitude per mile
-  
+
     const lowerLat = latitude - lat * distance;
     const lowerLon = longitude - lon * distance;
-  
+
     const upperLat = latitude + lat * distance;
     const upperLon = longitude + lon * distance;
-  
+
     const lower = geohash.encode(lowerLat, lowerLon);
     const upper = geohash.encode(upperLat, upperLon);
-  
+
     return {
       lower,
       upper
     };
   };
-  
+
   //const firestore = new Firestore();
   const db = firebase.firestore();
 
@@ -107,112 +97,137 @@ export default function Accidents(props) {
     return snapShot.docs.length;
   }
   const watch = true;
-  const {latitude,
+  const { latitude,
     longitude,
     timestamp,
     accuracy,
-    error,} = usePosition(); 
-    
+    error, } = usePosition();
+
 
   //console.log(`longitude: ${lng} | latitude: ${lat}`);
-    //console.log("lat: " + latitude)
+  //console.log("lat: " + latitude)
 
-const[rows, setRows] = useState([]); 
-//let rows=[]; 
-useEffect(() => { 
-   // Retrieve the current coordinates using the navigator API
-   const randomLatitude = 9.4186961;
-   const randomLongitude = -0.8192849;
-   // Get a geohash range of 10 miles on all sides;
-   const range = getGeohashRange(randomLatitude, randomLongitude, 30);
- //rows = [];
- db
-   .collection("Accidents")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper)
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const listItems = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      date:new Date(doc.data().timestamp)
-      .toUTCString(),
-      ...doc.data(), //spread operator merges data to id. 
-    }));
-    setRows(listItems); //items is equal to listItems 
-    console.log(listItems);
+  const [rows, setRows] = useState([]);
+  //let rows=[]; 
+  useEffect(() => {
+    // Retrieve the current coordinates using the navigator API
+    const randomLatitude = 9.4186961;
+    const randomLongitude = -0.8192849;
+    // Get a geohash range of 10 miles on all sides;
+    const range = getGeohashRange(randomLatitude, randomLongitude, 30);
+    //rows = [];
+    db
+      .collection("Accidents")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const listItems = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          id: doc.id, //id and data pushed into items array 
+          date: new Date(doc.data().timestamp)
+            .toUTCString(),
+          ...doc.data(), //spread operator merges data to id. 
+        }));
+        setRows(listItems); //items is equal to listItems 
+        console.log(listItems);
+      });
+  }, []);
+
+  /**
+   * .get()
+  .then(snapshot => {
+     snapshot.forEach(doc => {   
+      //rows=[];         
+      //var snap = db.collection("Accidents").get().;  
+      var a= doc.data().info; 
+      var b= doc.data().timestamp; 
+      var query= createData(a,b);  
+      setRows([...rows, query]);
+      console.log(query); 
+      return query;  
+    }); 
+    //setRows(rows=>[...rows, ])
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
   });
-}, []);
-   
-   /**
-    * .get()
-   .then(snapshot => {
-      snapshot.forEach(doc => {   
-       //rows=[];         
-       //var snap = db.collection("Accidents").get().;  
-       var a= doc.data().info; 
-       var b= doc.data().timestamp; 
-       var query= createData(a,b);  
-       setRows([...rows, query]);
-       console.log(query); 
-       return query;  
-     }); 
-     //setRows(rows=>[...rows, ])
-   })
-   .catch(err => {
-     console.log('Error getting documents', err);
-   });
-    */
+   */
 
 
-   // .catch(function(error) {
-      //  console.log("Error getting documents: ", error);
-    //});
+  // .catch(function(error) {
+  //  console.log("Error getting documents: ", error);
+  //});
 
-     // createData(1, 'Accidents', '20-05-2020'),
-  
-  
+  // createData(1, 'Accidents', '20-05-2020'),
+
+
   const useStyles = makeStyles({
     table: {
       minWidth: 700,
     },
   });
-  
-  
+
+
   const classes = useStyles();
- //const { tableHead, tableData, tableHeaderColor } = props;
+  //const { tableHead, tableData, tableHeaderColor } = props;
   return (
     <div className={classes.tableResponsive}>
       <Container style={{ marginTop: '6em' }}>
-          <TableContainer
-            component={Paper}
-            style={{ marginTop: '2em', marginBottom: '6em' }}
-          >
-            <Table className={classes.table} aria-label='customized table'>
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell align='center'>Problem</StyledTableCell>
-                  <StyledTableCell align='center'>Date</StyledTableCell>
-                  <StyledTableCell align='center'>Actions</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow>
-                    <StyledTableCell align='center'>
-                      {row.info}
-                    </StyledTableCell>
-                    <StyledTableCell align='center'>{row.date}</StyledTableCell>
-                    <StyledTableCell align='center'>
-                      {switchButton}
-                      {button}
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Container>
+        <TableContainer
+          component={Paper}
+          style={{ marginTop: '2em', marginBottom: '6em' }}
+        >
+          <Table className={classes.table} aria-label='customized table'>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align='center'>Problem</StyledTableCell>
+                <StyledTableCell align='center'>Date</StyledTableCell>
+                <StyledTableCell align='center'>Actions</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+
+                <StyledTableRow >
+                  <StyledTableCell align='center'>
+                    {row.info}
+                  </StyledTableCell>
+                  <StyledTableCell align='center'>{row.date}</StyledTableCell>
+                  <StyledTableCell align='center'>
+                    <Button
+                      component={Link}
+                      variant='contained'
+                      color='secondary'
+                      size='small'
+                      onClick={() => updateContent(row)}
+                    //to={'/maps'} 
+
+                    >
+                      Solved
+    </Button>
+
+                    <Button
+                      component={Link}
+                      variant='contained'
+                      color='primary'
+                      size='small'
+                      to={{
+                        pathname: '/maps',
+                        state: {
+                          loc: row.location.geopoint
+                        }
+                      }}
+                    >
+                      View Map
+    </Button >
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 }
--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { compose, withProps } from 'recompose'; 
-import firebase from "firebase"; 
+import { compose, withProps } from 'recompose';
+import firebase from "firebase";
 import geohash from "ngeohash";
-import { number, array } from "yup"; 
+import { number, array } from "yup";
 import fetch from "isomorphic-fetch";
 import {
   withScriptjs,
@@ -11,222 +11,224 @@ import {
   Marker,
 } from 'react-google-maps';
 
-export default function Maps() { 
+export default function Maps(myprop) {
 
-const MapComponent = compose( 
-  withProps({
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=',
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `90vh`, marginTop: '2rem' }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-) 
-(props => { 
-
- 
-    function displayMarkers (){ 
-      return Object.values(marker).map((marker, index) => {
-        return <Marker key={index} id={index} position={{
-         lat:Number(marker.Ic),
-         lng:Number(marker.wc)
-       }} 
-       onClick={() => console.log("You clicked me!")} />
-      })
-    }
+  const MapComponent = compose(
+    withProps({
+      googleMapURL:
+        'https://maps.googleapis.com/maps/api/js?key=',
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `90vh`, marginTop: '2rem' }} />,
+      mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+  )
+    (props => {
 
 
- const mark= <GoogleMap  
-    defaultZoom={15}
-    defaultCenter={{ lat:  9.4186961, lng: -0.8192849 }}
-  >  
-  { displayMarkers() }
-  </GoogleMap> 
-  return mark;
-});  
+      function displayMarkers() {
+        console.log("inside")
+        let cordinates = myprop.location.state.loc
+        // return Object.values(marker).map((marker, index) => {
+          return <Marker key={1} id={1} position={{
+            lat: Number(cordinates.Ic),
+            lng: Number(cordinates.wc)
+          }}
+            onClick={() => console.log("You clicked me!")} />
+        // })
+      }
 
 
-const getGeohashRange = ( 
-  latitude= number,
-  longitude= number,
-  distance = number, // miles
-) => {
-  const lat = 0.0144927536231884; // degrees latitude per mile
-  const lon = 0.0181818181818182; // degrees longitude per mile
+      const mark = <GoogleMap
+        defaultZoom={15}
+        defaultCenter={{ lat: 9.4186961, lng: -0.8192849 }}
+      >
+        {displayMarkers()}
+      </GoogleMap>
+      return mark;
+    });
 
-  const lowerLat = latitude - lat * distance;
-  const lowerLon = longitude - lon * distance;
 
-  const upperLat = latitude + lat * distance;
-  const upperLon = longitude + lon * distance;
+  const getGeohashRange = (
+    latitude = number,
+    longitude = number,
+    distance = number, // miles
+  ) => {
+    const lat = 0.0144927536231884; // degrees latitude per mile
+    const lon = 0.0181818181818182; // degrees longitude per mile
 
-  const lower = geohash.encode(lowerLat, lowerLon);
-  const upper = geohash.encode(upperLat, upperLon);
+    const lowerLat = latitude - lat * distance;
+    const lowerLon = longitude - lon * distance;
 
-  return {
-    lower,
-    upper
+    const upperLat = latitude + lat * distance;
+    const upperLon = longitude + lon * distance;
+
+    const lower = geohash.encode(lowerLat, lowerLon);
+    const upper = geohash.encode(upperLat, upperLon);
+
+    return {
+      lower,
+      upper
+    };
   };
-};
 
 
 
-    
-function createData(long,lat) {
-  return { long, lat };
-}
 
-const db = firebase.firestore();
-let [marker, setMarker] = useState([]);
-    const { isMarkerShown } = false;
-    useEffect(() => {  
-       // Retrieve the current coordinates using the navigator API
-   const randomLatitude = 9.4186961;
-   const randomLongitude = -0.8192849;
-   // Get a geohash range of 10 miles on all sides;
-   const range = getGeohashRange(randomLatitude, randomLongitude, 30);
- //rows = [];
- db
-   .collection("Accidents")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper) 
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-    setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker[x-1]);
-     }); 
-     
-     db
-    .collection("Robbery")
-    .where("location.geohash", ">=", range.lower)
-    .where("location.geohash", "<=", range.upper) 
-    .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-     setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker);
-     });  
+  function createData(long, lat) {
+    return { long, lat };
+  }
 
-     db
+  const db = firebase.firestore();
+  let [marker, setMarker] = useState([]);
+  const { isMarkerShown } = false;
+  useEffect(() => {
+    // Retrieve the current coordinates using the navigator API
+    const randomLatitude = 9.4186961;
+    const randomLongitude = -0.8192849;
+    // Get a geohash range of 10 miles on all sides;
+    const range = getGeohashRange(randomLatitude, randomLongitude, 30);
+    //rows = [];
+    db
+      .collection("Accidents")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker[x - 1]);
+      });
+
+    db
+      .collection("Robbery")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker);
+      });
+
+    db
       .collection("Potholes")
       .where("location.geohash", ">=", range.lower)
-      .where("location.geohash", "<=", range.upper) 
+      .where("location.geohash", "<=", range.upper)
       .onSnapshot(snapshot => {
-      //You can "listen" to a document with the onSnapshot() method.
-      const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-      setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-      let x= marker.length;
-      console.log(marker);
-     });  
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker);
+      });
 
-     db
-   .collection("Blockage")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper) 
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-    setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker[x-1]);
-     });  
+    db
+      .collection("Blockage")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker[x - 1]);
+      });
 
-     db
-   .collection("Traffic")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper) 
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-    setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker[x-1]);
-     });  
+    db
+      .collection("Traffic")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker[x - 1]);
+      });
 
-     db
-   .collection("Street_lights")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper) 
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-    setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker);
-     }); 
+    db
+      .collection("Street_lights")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker);
+      });
 
-     db
-   .collection("Car_breakdown")
-   .where("location.geohash", ">=", range.lower)
-   .where("location.geohash", "<=", range.upper) 
-   .onSnapshot(snapshot => {
-    //You can "listen" to a document with the onSnapshot() method.
-    const location = snapshot.docs.map(doc => ({
-      //map each document into snapshot
-      //id: doc.id, //id and data pushed into items array 
-      ...doc.data().location.geopoint, //spread operator merges data to id. 
-    })); 
-    setMarker(marker=location);  //items is equal to listItems  
-    //marker=location 
-    let x= marker.length;
-    console.log(marker[x-1]);
-     }); 
+    db
+      .collection("Car_breakdown")
+      .where("location.geohash", ">=", range.lower)
+      .where("location.geohash", "<=", range.upper)
+      .onSnapshot(snapshot => {
+        //You can "listen" to a document with the onSnapshot() method.
+        const location = snapshot.docs.map(doc => ({
+          //map each document into snapshot
+          //id: doc.id, //id and data pushed into items array 
+          ...doc.data().location.geopoint, //spread operator merges data to id. 
+        }));
+        setMarker(marker = location);  //items is equal to listItems  
+        //marker=location 
+        let x = marker.length;
+        console.log(marker[x - 1]);
+      });
 
-        delayedShowMarker();
-    }, []);
+    delayedShowMarker();
+  }, []);
 
-    const delayedShowMarker = () => {
-        setMarker({ isMarkerShown: true });
-    };
+  const delayedShowMarker = () => {
+    setMarker({ isMarkerShown: true });
+  };
 
-    const handleMarkerClick = () => {
-        setMarker({ isMarkerShown: false });
-        delayedShowMarker();
-    };
+  const handleMarkerClick = () => {
+    setMarker({ isMarkerShown: false });
+    delayedShowMarker();
+  };
 
-    return (
-      
-      <div> 
-        <MapComponent
-          isMarkerShown={isMarkerShown}
-          onMarkerClick={handleMarkerClick}
-        />
-      </div>
-    );
-  }
-    
+  return (
+
+    <div>
+      <MapComponent
+        isMarkerShown={isMarkerShown}
+        onMarkerClick={handleMarkerClick}
+      />
+    </div>
+  );
+}
+
